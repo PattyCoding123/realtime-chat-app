42276b2e0278269c379f1aff705d25e0c7f01556
Patrick Ducusin <pducusin123@gmail.com>
2023-04-10 14:35:20 -0400

    Add data storing for redis

---
+++ src/app/api/checkid/route.ts
@@ -0,0 +1,25 @@
+import { getAuth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { NextApiRequest } from "next";
+
+import { fetchRedis } from "@/lib/helpers/redisFetch";
+import { db } from "@/lib/db";
+
+export async function GET(req: NextApiRequest) {
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const userExists = await db.sismember(
+    `user:${userId}`,
+    "incoming_friend_request"
+  );
+
+  if (!userExists) {
+    // If user does not exist, store user information in Upstash Redis
+    await db.hset(`user:${userId}`, { incoming_friend_requests: [] });
+  }
+  redirect("/");
+}

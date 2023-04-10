e79f7c9a182706f656a5fafc19a49fde74ec4992
Patrick Ducusin <pducusin123@gmail.com>
2023-04-10 14:41:55 -0400

    add try-catch in checkid api route

--- src/app/api/checkid/route.ts
+++ src/app/api/checkid/route.ts
@@ -6,21 +6,25 @@ import { fetchRedis } from "@/lib/helpers/redisFetch";
 import { db } from "@/lib/db";
 
 export async function GET(req: NextApiRequest) {
-  const { userId } = getAuth(req);
+  try {
+    const { userId } = getAuth(req);
 
-  if (!userId) {
-    redirect("/sign-in");
+    if (!userId) {
+      redirect("/sign-in"); // If user is not signed in, redirect to sign-in page
+    }
+
+    const userExists = (await fetchRedis(
+      "sismember",
+      `user:${userId}`,
+      "incoming_friend_requests"
+    )) as 0 | 1;
+
+    if (!userExists) {
+      // If user does not exist, store user information in Upstash Redis
+      await db.hset(`user:${userId}`, { incoming_friend_requests: [] });
+    }
+    redirect("/");
+  } catch (error) {
+    return new Response("Invalid Request", { status: 400 });
   }
-
-  const userExists = (await fetchRedis(
-    "sismember",
-    `user:${userId}`,
-    "incoming_friend_requests"
-  )) as 0 | 1;
-
-  if (!userExists) {
-    // If user does not exist, store user information in Upstash Redis
-    await db.hset(`user:${userId}`, { incoming_friend_requests: [] });
-  }
-  redirect("/");
 }

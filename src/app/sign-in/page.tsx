5192bc37c11576307d30827ece6673d308d11c1f
Patrick <pducusin123@gmail.com>
2023-04-10 22:06:02 -0400

    Remove redirect url from signin/up

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs/app-beta";
 export default function Page() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <SignIn signUpUrl="/sign-up" redirectUrl={"/api/checkid"} />;
+      <SignIn signUpUrl="/sign-up" />;
     </div>
   );
 }

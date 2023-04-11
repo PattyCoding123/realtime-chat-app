db8554cc75fe32170db1086e4579246cc5bf0c7b
Patrick <pducusin123@gmail.com>
2023-04-10 22:36:51 -0400

    No need to return sadd

--- src/lib/helpers/userIdExists.ts
+++ src/lib/helpers/userIdExists.ts
@@ -8,12 +8,7 @@ export default async function userIdExists(userId: string) {
 
     if (!userExists) {
       // If user does not exist, store user information in Upstash Redis
-      const res = await db.sadd(
-        `user:${userId}`,
-        "friends",
-        "incoming_friend_requests"
-      );
-      return res;
+      await db.sadd(`user:${userId}`, "friends", "incoming_friend_requests");
     }
   } catch (error) {
     throw new Error(`Error executing Redis command`);

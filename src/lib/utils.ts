5eb61a90b52ecf26bbabf93e239125284685f41d
Patrick Ducusin <pducusin123@gmail.com>
2023-04-12 19:04:53 -0400

    Add an idValidator

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,6 +8,10 @@ export function cn(...inputs: ClassValue[]): string {
 }
 
 // Validator function for the add friend form
-export const addFriendValidator = z.object({
+export const emailValidator = z.object({
   email: z.string().email(),
 });
+
+// Validator function for the id
+
+export const idValidator = z.object({ id: z.string() });

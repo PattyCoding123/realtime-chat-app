e69ff86ec612b845646170ac1a11a725cdf57475
Patrick Ducusin <pducusin123@gmail.com>
2023-04-12 18:51:46 -0400

    work on requests page and FriendRequests page

---
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -0,0 +1,50 @@
+import { notFound } from "next/navigation";
+import { clerkClient, currentUser } from "@clerk/nextjs/app-beta";
+import type { User } from "@clerk/nextjs/api";
+
+import { fetchRedis } from "@/lib/helpers/fetchRedis";
+import FriendRequests from "@/components/FriendRequests";
+
+interface PageProps {}
+
+const FIRST_EMAIL_INDEX = 0;
+
+const Page = async ({}: PageProps) => {
+  const sessionUser: User | null = await currentUser();
+
+  if (!sessionUser) notFound();
+
+  // ids of people who sent logged in user a friend request
+  const incomingSenderIds = (await fetchRedis(
+    "smembers",
+    `user:${sessionUser.id}:incoming_friend_requests`
+  )) as string[];
+
+  // get emails of people who sent logged in user a friend request
+  const userEmails = await clerkClient.users
+    .getUserList({
+      userId: incomingSenderIds,
+    })
+    .then((res) =>
+      res.map((user) => {
+        return {
+          senderId: user.id,
+          senderEmail: user.emailAddresses[FIRST_EMAIL_INDEX].emailAddress,
+        };
+      })
+    );
+
+  return (
+    <main className="p-8">
+      <h1 className="mb-8 text-5xl font-bold">Add a friend</h1>
+      <div className="flex flex-col gap-4">
+        <FriendRequests
+          incomingFriendRequests={userEmails}
+          sessionUserId={sessionUser.id}
+        />
+      </div>
+    </main>
+  );
+};
+
+export default Page;
---
+++ src/components/FriendRequests.tsx
@@ -0,0 +1,75 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { Check, UserPlus, X } from "lucide-react";
+import { FC, useState } from "react";
+
+interface FriendRequestsProps {
+  sessionUserId: string;
+  incomingFriendRequests: IncomingFriendRequest[];
+}
+
+const FriendRequests: FC<FriendRequestsProps> = ({
+  incomingFriendRequests,
+  sessionUserId,
+}) => {
+  const router = useRouter();
+  const [friendRequests, setFriendRequests] = useState<IncomingFriendRequest[]>(
+    incomingFriendRequests
+  );
+
+  // Callback to call the acceptFriend route handler
+  const acceptFriend = async (senderId: string) => {
+    await axios.post("/api/friends/accept", { id: senderId });
+
+    // Remove the friend request from the list
+    setFriendRequests((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+
+    router.refresh();
+  };
+
+  // Callback to call the denyFriend route handler
+  const denyFriend = async (senderId: string) => {
+    await axios.post("/api/friends/deny", { id: senderId });
+
+    // Remove the friend request from the list
+    setFriendRequests((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+
+    router.refresh();
+  };
+
+  return (
+    <>
+      {friendRequests.length === 0 ? (
+        <p className="text-sm text-zinc-500">
+          No friend requests at the moment.
+        </p>
+      ) : (
+        friendRequests.map((request) => (
+          <div key={request.senderId} className="flex items-center gap-4">
+            <UserPlus className="text-black" />
+            <p className="text-lg font-medium">{request.senderEmail}</p>
+            <button
+              aria-label="accept friend"
+              className="grid h-8 w-8 place-content-center rounded-full bg-indigo-600 transition hover:bg-indigo-700 hover:shadow-md"
+            >
+              <Check className="h-11/12 w-11/12 font-semibold text-white" />
+            </button>
+            <button
+              aria-label="deny friend"
+              className="grid h-8 w-8 place-content-center rounded-full bg-red-600 transition hover:bg-red-700 hover:shadow-md"
+            >
+              <X className="h-11/12 w-11/12 font-semibold text-white" />
+            </button>
+          </div>
+        ))
+      )}
+    </>
+  );
+};
+
+export default FriendRequests;

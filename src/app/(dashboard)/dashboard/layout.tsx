6a41ae832c9ea8557d604515d128823acd063035
Patrick Ducusin <pducusin123@gmail.com>
2023-04-12 16:49:19 -0400

    Add helpful comments in layout.tsx

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -10,6 +10,8 @@ interface LayoutProps {
   children: ReactNode;
 }
 
+// The Icon type will be the union of
+// of the keys of the Icons
 interface SidebarOption {
   id: number;
   name: string;
@@ -17,6 +19,7 @@ interface SidebarOption {
   Icon: Icon;
 }
 
+// Add more sidebar options as needed
 const sidebarOptions: SidebarOption[] = [
   {
     id: 1,
@@ -31,11 +34,14 @@ const FIRST_EMAIL_INDEX = 0;
 const Layout = async ({ children }: LayoutProps) => {
   const user: User | null = await currentUser();
   return (
+    // Separated into two sections: sidebar and main content
     <div className="flex h-screen w-full">
+      {/* Sidebar */}
       <div
         className="flex h-full w-full max-w-xs flex-col gap-y-5 overflow-y-auto 
       border-r border-gray-200 bg-white px-6"
       >
+        {/* Logo */}
         <div className="flex items-center justify-between">
           <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
             <Icons.Logo className="h-8 w-8" />
@@ -48,12 +54,16 @@ const Layout = async ({ children }: LayoutProps) => {
 
         <nav className="flex flex-1 flex-col">
           <ul role="list" className="flex flex-1 flex-col gap-y-7">
+            {/* Render all available chats */}
             <li>Chats that this user has</li>
+
+            {/* Render all available options */}
             <li>
               <div className="text-xs font-semibold leading-6 text-gray-400">
                 Overview
               </div>
 
+              {/* Render all sidebar options */}
               <ul role="list" className="-mx-2 mt-2 space-y-1">
                 {sidebarOptions.map((option) => {
                   const Icon = Icons[option.Icon];
@@ -79,6 +89,7 @@ const Layout = async ({ children }: LayoutProps) => {
 
             {/* mt-auto pushes this section to the bottom of sidebar */}
             <li className="-mx-6 mt-auto flex items-center">
+              {/* Show profile button and info, powered with Clerk */}
               <div className="text flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold text-gray-900">
                 <SignedIn>
                   <UserButton afterSignOutUrl="/sign-in" signInUrl="/sign-in" />
@@ -96,6 +107,7 @@ const Layout = async ({ children }: LayoutProps) => {
           </ul>
         </nav>
       </div>
+      {/* Main content from other pages */}
       {children}
     </div>
   );

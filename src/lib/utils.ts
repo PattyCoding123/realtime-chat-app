10459f9ed88d66783b1f0377e9ccb56a1dceab80
Patrick Ducusin <pducusin123@gmail.com>
2023-04-15 17:31:37 -0400

    Create helper function for pusher

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,16 @@ export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
 
+/* This is a helper function to create the dynamic link segments
+   for the chat pages. It takes two user IDs and returns a string.
+*/
 export function chatHrefConstructor(id1: string, id2: string): string {
   const sortedIds = [id1, id2].sort();
 
   return `${sortedIds[0]}--${sortedIds[1]}`;
+}
+
+// Helper function to convert a redis key string to a valid Pusher key
+export function toPusherKey(key: string): string {
+  return key.replace(/:/g, "__");
 }

419410374f0483eade256c5ed58fa86a5c976c66
Patrick Ducusin <pducusin123@gmail.com>
2023-04-15 14:29:53 -0400

    update next.config.js to allow images from clerk

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

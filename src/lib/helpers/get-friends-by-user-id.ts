6ace8b7bbfcd17b53f42c8b86026642aa7fd4a49
Patrick Ducusin <pducusin123@gmail.com>
2023-04-15 13:21:21 -0400

    Create helper function to get user's friends

---
+++ src/lib/helpers/get-friends-by-user-id.ts
@@ -0,0 +1,19 @@
+import { clerkClient } from "@clerk/nextjs/app-beta";
+import { fetchRedis } from "./fetchRedis";
+import type { User } from "@clerk/nextjs/api";
+
+export const getFriendsByUserId = async (userId: string): Promise<User[]> => {
+  // Retrieve the friends of the current user
+  const friendIds = (await fetchRedis(
+    "smembers",
+    `user:${userId}:friends`
+  )) as string[];
+
+  // Enrich the friends with their details using Clerk's user API
+  const friends =
+    friendIds.length > 0
+      ? await clerkClient.users.getUserList({ userId: friendIds })
+      : [];
+
+  return friends;
+};

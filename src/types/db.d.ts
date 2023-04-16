e4dd01dccf7429789cfc2508aeac8ce419d58d3a
Patrick Ducusin <pducusin123@gmail.com>
2023-04-16 13:12:52 -0400

    Remove interface Message as we now are using the zod validator

--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -3,14 +3,6 @@ interface Chat {
   messages: Message[];
 }
 
-interface Message {
-  id: string;
-  senderId: string;
-  receiverId: string;
-  text: string;
-  timestamp: number;
-}
-
 interface FriendRequest {
   id: string;
   senderId: string;

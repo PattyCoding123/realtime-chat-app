f66ba64f8a2447426d3e2ab935bf045252db9647
Patrick Ducusin <pducusin123@gmail.com>
2023-04-16 17:27:51 -0400

    Update middleware and sign in component

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs/app-beta";
 export default function Page() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <SignIn signUpUrl="/sign-up" />;
+      <SignIn signUpUrl="/sign-up" afterSignInUrl="/dashboard" />;
     </div>
   );
 }

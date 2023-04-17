c941dd06f1ebe137676bce8d9c15c36fc7b18af9
Patrick <pducusin123@gmail.com>
2023-04-16 23:44:56 -0400

    Always render "Your chats"

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -75,11 +75,10 @@ const Layout = async ({ children }: LayoutProps) => {
         </div>
 
         {/* Render "Your chats" if user has friends */}
-        {friends.length > 0 ? (
-          <div className="text-xs font-semibold leading-6 text-gray-400">
-            Your chats
-          </div>
-        ) : null}
+
+        <div className="text-xs font-semibold leading-6 text-gray-400">
+          Your chats
+        </div>
 
         <nav className="flex flex-1 flex-col">
           <ul role="list" className="flex flex-1 flex-col gap-y-7">
